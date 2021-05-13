2adefb540ed08586f171a0150ce4176adfa2ecb6
Hang <hang.yin2017@outlook.com>
2021-05-13 22:01:04 +1000

    Separated constants

---
+++ constants.js
@@ -0,0 +1,48 @@
+/**
+* @typedef TaxTier
+* @property {number} min Maximum income of this tier.
+* @property {number} max Minimum income of this tier.
+* @property {number} floor Equals to the max of the lower tier.
+* @property {number} base Accumulated tax from lower tiers.
+* @property {number} rate Tax rate of this tier.
+*/
+/**
+* @type {TaxTier[]}
+*/
+export const TAX_TABLE = [
+  {
+    min: 0,
+    max: 20000,
+    floor: 0,
+    base: 0,
+    rate: 0,
+  },
+  {
+    min: 20001,
+    max: 40000,
+    floor: 20000,
+    base: 0,
+    rate: 0.1,
+  },
+  {
+    min: 40001,
+    max: 80000,
+    floor: 40000,
+    base: 2000,
+    rate: 0.2,
+  },
+  {
+    min: 80001,
+    max: 180000,
+    floor: 80000,
+    base: 10000,
+    rate: 0.3,
+  },
+  {
+    min: 180001,
+    max: Number.POSITIVE_INFINITY,
+    floor: 180000,
+    base: 40000,
+    rate: 0.4,
+  },
+];
--- main.js
+++ main.js
@@ -1,55 +1,9 @@
 "use strict";
 
-const prompt = require("prompt-sync")({ sigint: true });
+import promptSync from 'prompt-sync';
+import { TAX_TABLE } from './constants.js';
 
-/**
- * @typedef TaxTier
- * @property {number} min Maximum income of this tier.
- * @property {number} max Minimum income of this tier.
- * @property {number} floor Equals to the max of the lower tier.
- * @property {number} base Accumulated tax from lower tiers.
- * @property {number} rate Tax rate of this tier.
- */
-/**
- * @type {TaxTier[]}
- */
-const TAX_TABLE = [
-  {
-    min: 0,
-    max: 20000,
-    floor: 0,
-    base: 0,
-    rate: 0,
-  },
-  {
-    min: 20001,
-    max: 40000,
-    floor: 20000,
-    base: 0,
-    rate: 0.1,
-  },
-  {
-    min: 40001,
-    max: 80000,
-    floor: 40000,
-    base: 2000,
-    rate: 0.2,
-  },
-  {
-    min: 80001,
-    max: 180000,
-    floor: 80000,
-    base: 10000,
-    rate: 0.3,
-  },
-  {
-    min: 180001,
-    max: Number.POSITIVE_INFINITY,
-    floor: 180000,
-    base: 40000,
-    rate: 0.4,
-  },
-];
+const prompt = promptSync({ sigint: true });
 
 /**
  * @description
